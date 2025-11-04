bfb3b948e969257a988ac13b71194ff7bca4f2a7
Sandaru03 <sandarudilshan24@gmail.com>
2025-11-04 13:54:30 +0530

    email_config.php edit

--- application/config/email_config.php
+++ application/config/email_config.php
@@ -3,8 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['protocol']    = 'smtp';
 $config['smtp_host']   = 'smtp.gmail.com';
-$config['smtp_user']   = getenv('SMTP_USER'); 
-$config['smtp_pass']   = getenv('SMTP_PASS'); 
+$config['smtp_user']   = 'sandarudilshan24@gmail.com';  
+$config['smtp_pass']   = 'kvnpzjygkkdeosqp'; 
 $config['smtp_port']   = 587;
 $config['smtp_crypto'] = 'tls';
 $config['mailtype']    = 'html';
